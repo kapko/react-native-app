536beb1c4880c368c71f9732464f2ba4b53e5367
kzhekshen <kzhekshen@incentient.com>
2018-03-06 09:55:06 +0600

    description fixed

--- app/components/card.js
+++ app/components/card.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, View } from 'react-native';
 import { Content, Card, CardItem, Body, Right, Thumbnail, Text, Button, Left, Title, H3, H2, Icon } from 'native-base';
-// import Icon from '../../node_modules/react-native-vector-icons/FontAwesome'
+import { Entypo, MaterialCommunityIcons, FontAwesome } from "@expo/vector-icons"
+import styles from './styles';
 
 export class CardComponent extends React.Component {
   render() {
@@ -11,27 +12,26 @@ export class CardComponent extends React.Component {
           <CardItem cardBody>
             <Image source={item.src} style={{height: 200, width: null, flex: 1}}/>
           </CardItem>
-          <CardItem>
+          <CardItem style={styles.cartHeader}>
             <Left><H3>Цена:{item.price} {item.currency}</H3></Left>
             <Right><H3>{item.typeOfRoom}</H3></Right>
           </CardItem>
-          {/* <Content style={{paddingHorizontal:15}}>
-            <Text>
-              <Icon name='home' style={{fontSize: 20, color: 'red'}}/> {item.situatited}</Text>
-            <Text>
-              <Icon name='home' style={{fontSize: 20, color: 'red'}}/>
-              {item.squeire}</Text>
-            <Text>
-              <Icon name='home' style={{fontSize: 20, color: 'red'}}/>
-              {item.adress}</Text>
-            <Text>
-              <Icon name='home' style={{fontSize: 20, color: 'red'}}/>
-              {item.floor} / {item.floorOf}</Text>
-          </Content> */}
-          <CardItem footer>
-            <Text>
-              <Icon ios='ios-menu' android="md-menu" style={{fontSize: 20, color: 'red'}}/>
-              {item.typeSeller}</Text>
+          <Content style={{paddingHorizontal:10}}>
+            <Button transparent>
+              <Text style={styles.cardText}>* {item.room}к.кв.</Text>
+              <Text style={styles.cardText}>* {item.squeire} m&sup2;</Text>
+              <Text style={styles.cardText}>* {item.floor}/{item.floorOf}этаж</Text>
+            </Button>
+          </Content>
+          <CardItem footer style={styles.cartFooter}>
+            <Button transparent>
+              <MaterialCommunityIcons style={styles.cardIconFooterLeft} name="map-marker"/>
+              <Text style={styles.cardText}>{item.situatited}</Text>
+            </Button>
+            <Button transparent>
+              <FontAwesome style={styles.cardIconFooter} name={(item.typeSeller === 'Владелец') ? 'user' : 'user-times'}/>
+              <Text style={styles.cardText}>{item.typeSeller}</Text>
+            </Button>
           </CardItem>
         </Card>
       );
---
+++ app/components/styles.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+
+export default styles = StyleSheet.create({
+  cardIcon: {
+    fontSize: 20,
+    paddingLeft: 0,
+    paddingRight: 0,
+    // paddingTop: 10,
+    color: '#333',
+  },
+  cardIconFooter: {
+    fontSize: 20,
+    paddingRight: 5,
+    // paddingTop: 3,
+  },
+  cardIconFooterLeft: {
+    fontSize: 25,
+    paddingRight: 5,
+    paddingTop: 3,
+  },
+  cardButton: {
+    paddingTop: 0,
+    marginTop: 0,
+    paddingBottom: 0,
+  },
+  cartHeader: {
+    borderBottomColor: '#dfdfdf',
+    borderBottomWidth: 1,
+  },
+  cartFooter: {
+    paddingTop: 0,
+    marginTop: -5,
+    // borderTopColor: '#dfdfdf',
+    // borderTopWidth: 1,
+  },
+  cardText: {
+    paddingLeft: 0,
+    color: '#333'
+  }
+});
